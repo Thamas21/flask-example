5ce61f8e3d90fb58af268cea0955828ce83bbcda
Thomas Thelen-Clemmons <thomasthelen-clemmons@Thomass-MacBook-Air.local>
2022-03-28 15:33:24 -0700

    Added templates

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 
 @application.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
