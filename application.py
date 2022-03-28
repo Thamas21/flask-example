dc15152c03e3be0c14d4f6db821ca5be47f65a91
Thomas Thelen-Clemmons <thomasthelen-clemmons@Thomass-MacBook-Air.local>
2022-03-28 16:27:44 -0700

    added more stuff to problem solve

--- application.py
+++ application.py
@@ -2,7 +2,24 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
+db = SQLAlchemy(application)
 
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column('Project Name', db.String())
+    date = db.Column('Completion Date', db.DateTime())
+    description = db.Column('Description', db.Text)
+    skills_list = db.Column('Skills', db.String())
+    github = db.Column('GitHub Link', db.String())
+
+    def __repr__(self):
+        return f'''<Project(Project Title: {self.title},
+                Date: {self.date}
+                Description: {self.discription},
+                Skills List: {self.skills_list},
+                Github Link: {self.github})'''
 
 @application.route('/')
 def index():
@@ -10,4 +27,5 @@ def index():
 
 
 if __name__ == '__main__':
+    db.create_all()
     application.run()
